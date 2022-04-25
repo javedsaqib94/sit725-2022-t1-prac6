57e8a549356519b52e687691e0d66b08d30a5c61
javedsaqib94 <javed.saqib94@gmail.com>
2022-04-26 01:00:43 +1000

    run 2 terminals

--- test/test.js
+++ test/test.js
@@ -1,43 +1,68 @@
 var expect  = require("chai").expect;
 var request = require("request");
 
-  describe("Add strings and number", function() {
-    var url = "http://localhost:3000/addTwoNumbers/a/5";
-    it("should return status 200", function(done) {
+describe("Add string and number", function() {
+  let url = "http://localhost:3000/addTwoNumbers/a/3";
+  it("should return status 200", function(done) {
       request(url, function(error, response, body) {
           expect(response.statusCode).to.equal(200);
           done()
         });
   });
-    it("returns statusCode key in body to check if api gives right result should be 400", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.statusCode).to.equal(400);
-            done()
-          });
-    });
-    it("returns the result as null", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.result).to.be.a('null');
-            done()
-          });
-    });
+  it("returns statusCode key in body to check if api gives right result should be 400", function(done) {
+      request(url, function(error, response, body) {
+          body = JSON.parse(body)
+          expect(body.statusCode).to.equal(400);
+          done()
+        });
   });
+  it("returns the result as null", function(done) {
+      request(url, function(error, response, body) {
+          body = JSON.parse(body)
+          expect(body.result).to.be.a('null');
+          done()
+        });
+  });
+});
 
-  describe("Add strings and speacial character", function() {
-    var url = "http://localhost:3000/addTwoNumbers/a/#";
-    it("should return status 200", function(done) {
-        request(url, function(error, response, body) {
-            expect(response.statusCode).to.equal(200);
-            done()
-          });
-    });
-    it("returns statusCode = 400", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.statusCode).to.equal(400);
-            done()
-          });
-    });
-  });
+
+  // describe("Add strings and number", function() {
+  //   var url = "http://localhost:3000/addTwoNumbers/a/5";
+  //   it("should return status 200", function(done) {
+  //     request(url, function(error, response, body) {
+  //         expect(response.statusCode).to.equal(200);
+  //         done()
+  //       });
+  // });
+  //   it("returns statusCode key in body to check if api gives right result should be 400", function(done) {
+  //       request(url, function(error, response, body) {
+  //           body = JSON.parse(body)
+  //           expect(body.statusCode).to.equal(400);
+  //           done()
+  //         });
+  //   });
+  //   it("returns the result as null", function(done) {
+  //       request(url, function(error, response, body) {
+  //           body = JSON.parse(body)
+  //           expect(body.result).to.be.a('null');
+  //           done()
+  //         });
+  //   });
+  // });
+
+  // describe("Add strings and speacial character", function() {
+  //   var url = "http://localhost:3000/addTwoNumbers/a/#";
+  //   it("should return status 200", function(done) {
+  //       request(url, function(error, response, body) {
+  //           expect(response.statusCode).to.equal(200);
+  //           done()
+  //         });
+  //   });
+  //   it("returns statusCode = 400", function(done) {
+  //       request(url, function(error, response, body) {
+  //           body = JSON.parse(body)
+  //           expect(body.statusCode).to.equal(400);
+  //           done()
+  //         });
+  //   });
+  // });
